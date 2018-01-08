0d5768921784b80187bb5e6bebbb59652fa6d73e
Cody.C.Walters <cody.c.walters@disney.com>
2018-01-08 14:37:40 -0800

    Report results use location data from the user

--- src/app.py
+++ src/app.py
@@ -2,15 +2,9 @@ from flask import Flask, render_template, request
 import requests
 import datetime
 
-HOST = 'data.kingcounty.gov'
-PREVDAYS = 360
-DATERANGE = str(datetime.date.today() - datetime.timedelta(days=PREVDAYS))
-QUERY = f'$select=name,address, inspection_date,violation_description&$where=inspection_date>"{DATERANGE}"&violation_type=red&$q="'
-
-
 def get_reports(host, query, business_name):
-	url = f'https://{host}/resource/gkhn-e8mn.json?{query}{business_name}"&$order=inspection_date,address DESC'
-	print(url)
+	url = f'https://{host}/resource/gkhn-e8mn.json?{query}&$q="{business_name}"'
+	print("fetching url: ", url)
 	r = requests.get(url)
 	if r.status_code is 200:
 		print("success")
@@ -19,11 +13,20 @@ def get_reports(host, query, business_name):
 		print("Not succesful")
 	return {'error': 'error getting reports ' + r.status_code}
 
+def get_user_zip():
+	geo_url = "http://freegeoip.net/json"
+	r = requests.get(geo_url)
+	geo_json = r.json()
+	return geo_json["zip_code"]
 
-
+HOST = 'data.kingcounty.gov'
+PREVDAYS = 360
+USER_ZIP = get_user_zip()
+DATERANGE = str(datetime.date.today() - datetime.timedelta(days=PREVDAYS))
+QUERY = f'$select=name,address,inspection_date,violation_description&$where=inspection_date>"{DATERANGE}"&violation_type=red$zip_code="{USER_ZIP}"&$order=inspection_date,address DESC'
 
 app = Flask(__name__)
-app.secrete_key = "hello"
+app.secrete_key = ""
 
 
 @app.route('/')
@@ -36,11 +39,12 @@ def reports_method():
 	name = request.form['name']
 	reports = get_reports(HOST, QUERY, name)
 	if (name == ''):
-		return render_template('result.html', reports="No Reports Found")
+		return render_template('result.html', table_header="No Reports Found")
 	if (len(reports) > 0):
-		return render_template('result.html', reports=reports, name=name)
+		name = "Reports for " + "'" + name + "'"
+		return render_template('result.html', reports=reports, table_header=name)
 
-	return render_template('result.html', reports="No Reports Found")
+	return render_template('result.html', table_header="No Reports Found")
 
 
 
