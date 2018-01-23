46379c1311eb42156b885f9f294cd9c067edee2f
Cody.C.Walters <cody.c.walters@disney.com>
2018-01-23 15:40:20 -0800

    Returned results use users location

--- src/app.py
+++ src/app.py
@@ -1,29 +1,43 @@
 from flask import Flask, render_template, request
 import requests
 import datetime
+import json
+
 
 def get_reports(host, query, business_name):
-	url = f'https://{host}/resource/gkhn-e8mn.json?{query}&$q="{business_name}"'
-	print("fetching url: ", url)
-	r = requests.get(url)
-	if r.status_code is 200:
-		print("success")
-		return r.json()
-	else:
-		print("Not succesful")
-	return {'error': 'error getting reports ' + r.status_code}
+    url = f'https://{host}/resource/gkhn-e8mn.json?{query}&$q="{business_name}"'
+    print("fetching url: ", url)
+    r = requests.get(url)
+    if r.status_code is 200:
+        print("success")
+        return r.json()
+    else:
+        print("Not successful")
+    return {'error': 'error getting reports ' + r.status_code}
 
-def get_user_zip():
-	geo_url = "http://freegeoip.net/json"
-	r = requests.get(geo_url)
-	geo_json = r.json()
-	return geo_json["zip_code"]
+
+def getUserLocation():
+    location_url = 'http://freegeoip.net/json'
+    r = requests.get(location_url)
+    j = json.loads(r.text)
+    lat = j['latitude']
+    lon = j['longitude']
+    location = {'lat': lat, 'lon': lon}
+    print("User Location: ", lat, lon)
+    return location
+
 
 HOST = 'data.kingcounty.gov'
 PREVDAYS = 360
-USER_ZIP = get_user_zip()
+BOUND_RANGE = .050
 DATERANGE = str(datetime.date.today() - datetime.timedelta(days=PREVDAYS))
-QUERY = f'$select=name,address,inspection_date,violation_description&$where=inspection_date>"{DATERANGE}"&violation_type=red&$order=inspection_date,address DESC'
+USER_LOCATION = getUserLocation()
+LAT_LOWER = USER_LOCATION['lat'] - BOUND_RANGE
+LAT_UPPER = USER_LOCATION['lat'] + BOUND_RANGE
+LON_LOWER = USER_LOCATION['lon'] - BOUND_RANGE
+LON_UPPER = USER_LOCATION['lon'] + BOUND_RANGE
+
+QUERY = f'$select=name,address,inspection_date,violation_description&violation_type=red&$where=inspection_date>"{DATERANGE}" AND latitude>{LAT_LOWER} AND latitude<{LAT_UPPER} AND longitude>{LON_LOWER} AND longitude<{LON_UPPER}&$order=inspection_date,address DESC'
 
 app = Flask(__name__)
 app.secrete_key = ""
@@ -36,17 +50,16 @@ def home_method():
 
 @app.route('/result', methods=['POST'])
 def reports_method():
-	name = request.form['name']
-	reports = get_reports(HOST, QUERY, name)
-	if (name == ''):
-		return render_template('result.html', table_header="No Reports Found", location = USER_ZIP)
-	if (len(reports) > 0):
-		name = "Reports for " + "'" + name + "'"
-		return render_template('result.html', reports=reports, table_header=name, location = USER_ZIP)
+    businessName = request.form['name']
+    reports = get_reports(HOST, QUERY, businessName)
+    if (businessName == ''):
+        return render_template('result.html', table_header="No Reports Found")
+    if (len(reports) > 0):
+        name = "Reports for " + "'" + businessName + "'"
+        return render_template('result.html', reports=reports, table_header=businessName)
 
-	return render_template('result.html', table_header="No Reports Found", location = USER_ZIP)
-
+    return render_template('result.html', table_header="No Reports Found")
 
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
