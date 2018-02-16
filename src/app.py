ab595373e0a001db7de2e528a5f6cdb04de5b90f
Cody.C.Walters <cody.c.walters@disney.com>
2018-02-16 10:32:58 -0800

    Moved to a single page application

--- src/app.py
+++ src/app.py
@@ -22,7 +22,8 @@ def getUserLocation():
     j = json.loads(r.text)
     lat = j['latitude']
     lon = j['longitude']
-    location = {'lat': lat, 'lon': lon}
+    # location = {'lat': lat, 'lon': lon}
+    location = {'lat': 47.4242534, 'lon': -122.1762584}
     print("User Location: ", lat, lon)
     return location
 
@@ -37,13 +38,11 @@ LAT_UPPER = USER_LOCATION['lat'] + BOUND_RANGE
 LON_LOWER = USER_LOCATION['lon'] - BOUND_RANGE
 LON_UPPER = USER_LOCATION['lon'] + BOUND_RANGE
 
-# Query issue when pushed to Heroku. Using different query to return answer for now
-# QUERY = f'$select=name,address,inspection_date,violation_description&violation_type=red&$where=inspection_date>"{DATERANGE}" AND latitude>{LAT_LOWER} AND latitude<{LAT_UPPER} AND longitude>{LON_LOWER} AND longitude<{LON_UPPER}&$order=inspection_date,address DESC'
-QUERY = f'$select=name,address,inspection_date,violation_description&violation_type=red&$where=inspection_date>"{DATERANGE}"&$order=inspection_date,address DESC'
+FIND_ALL_QUERY = f'$select=name,address,inspection_date,violation_description&violation_type=red&$where=inspection_date>"{DATERANGE}"&$order=inspection_date,address DESC'
+FIND_PROXIMITY_QUERY = f'$select=name,address,inspection_date,violation_description&violation_type=red&$where=inspection_date>"{DATERANGE}" AND latitude>{LAT_LOWER} AND latitude<{LAT_UPPER} AND longitude>{LON_LOWER} AND longitude<{LON_UPPER}&$order=inspection_date,address DESC'
 
 
 app = Flask(__name__)
-app.secrete_key = ""
 
 
 @app.route('/')
@@ -51,17 +50,24 @@ def home_method():
     return render_template('home.html')
 
 
-@app.route('/result', methods=['POST'])
+@app.route('/', methods=['POST'])
 def reports_method():
-    businessName = request.form['name']
-    reports = get_reports(HOST, QUERY, businessName)
-    if (businessName == ''):
-        return render_template('result.html', table_header="No Reports Found")
-    # if (len(reports) > 0):
-    name = "Reports for " + "'" + businessName + "'"
-    return render_template('result.html', reports=reports, table_header=businessName)
+    business_name = request.form['name']
+    if 'button_all' in request.form:
+        print('Button - Search All')
+        reports = get_reports(HOST, FIND_ALL_QUERY, business_name)
+        header_title = "All Reports for " + "'" + business_name + "'"
 
-    # return render_template('result.html', table_header="No Reports Found")
+    if 'button_location' in request.form:
+        print('Button - Search User Location')
+        reports = get_reports(HOST, FIND_PROXIMITY_QUERY, business_name)
+        header_title = "Reports in Proximity for " + "'" + business_name + "'"
+
+    return render_template('home.html', reports=reports, table_header=header_title, show_table=1)
+
+@app.route('/details')
+def details():
+    return render_template('details.html')
 
 
 if __name__ == '__main__':
