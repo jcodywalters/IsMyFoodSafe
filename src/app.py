b279bb89bb6f508e523e421a09eee3a50b15cc85
Cody Walters <jay_walters@discovery.com>
2020-01-03 00:24:12 -0800

    update api url, update queries, capitolize business name

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ import json
 
 
 def get_reports(host, query, business_name):
-    url = f'https://{host}/resource/gkhn-e8mn.json?{query}&$q="{business_name}"'
+    url = f'{host}?{query}&$q="{business_name.capitalize()}"'
     print("fetching url: ", url)
     r = requests.get(url)
     if r.status_code is 200:
@@ -17,18 +17,18 @@ def get_reports(host, query, business_name):
 
 
 def getUserLocation():
-    location_url = 'http://freegeoip.net/json'
-    r = requests.get(location_url)
-    j = json.loads(r.text)
-    lat = j['latitude']
-    lon = j['longitude']
-    location = {'lat': lat, 'lon': lon}
-    # location = {'lat': 47.4242534, 'lon': -122.1762584}  // Used for debugging
-    print("User Location: ", lat, lon)
+    # location_url = 'http://freegeoip.net/json' // this source is dead
+    # r = requests.get(location_url)
+    # j = json.loads(r.text)
+    # lat = j['latitude']
+    # lon = j['longitude']
+    # location = {'lat': lat, 'lon': lon}
+    location = {'lat': 47.4242534, 'lon': -122.1762584}
+    # print("User Location: ", lat, lon)
     return location
 
 
-HOST = 'data.kingcounty.gov'
+HOST = 'https://data.kingcounty.gov/resource/f29f-zza5.json'
 PREVDAYS = 360
 BOUND_RANGE = .10
 DATERANGE = str(datetime.date.today() - datetime.timedelta(days=PREVDAYS))
@@ -38,8 +38,8 @@ LAT_UPPER = USER_LOCATION['lat'] + BOUND_RANGE
 LON_LOWER = USER_LOCATION['lon'] - BOUND_RANGE
 LON_UPPER = USER_LOCATION['lon'] + BOUND_RANGE
 
-FIND_ALL_QUERY = f'$select=name,address,inspection_date,violation_description&violation_type=red&$where=inspection_date>"{DATERANGE}"&$order=inspection_date,address DESC'
-FIND_PROXIMITY_QUERY = f'$select=name,address,inspection_date,violation_description&violation_type=red&$where=inspection_date>"{DATERANGE}" AND latitude>{LAT_LOWER} AND latitude<{LAT_UPPER} AND longitude>{LON_LOWER} AND longitude<{LON_UPPER}&$order=inspection_date,address DESC'
+FIND_ALL_QUERY = f'$select=name,address,inspection_date,violation_description&violation_type=RED&$where=inspection_date>"{DATERANGE}"&$order=inspection_date,address DESC'
+FIND_PROXIMITY_QUERY = f'$select=name,address,inspection_date,violation_description&violation_type=RED&$where=inspection_date>"{DATERANGE}" AND latitude>{LAT_LOWER} AND latitude<{LAT_UPPER} AND longitude>{LON_LOWER} AND longitude<{LON_UPPER}&$order=inspection_date,address DESC'
 
 
 app = Flask(__name__)
